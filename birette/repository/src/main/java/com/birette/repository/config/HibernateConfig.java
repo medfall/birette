c38c035b1babd69d29a416a2fbed3bbf0effb0ea
Mohamed BEL <mohamed.bel@achatpublic.com>
2019-03-27 10:44:37 +0100

    hibernate configuration

---
+++ birette/repository/src/main/java/com/birette/repository/config/HibernateConfig.java
@@ -0,0 +1,43 @@
+package com.birette.repository.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@EnableAutoConfiguration
+public class HibernateConfig {
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+		localSessionFactoryBean.setDataSource(dataSource());
+		localSessionFactoryBean.setPackagesToScan(packagesToScan);
+		localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+
+	}
+
+	private Properties hibernateProperties() {
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+		return hibernateProperties;
+	}
+
+	@Bean
+	private DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+ 
+	}
+
+}
---
+++ birette/repository/src/main/java/com/birette/repository/entity/User.java
@@ -0,0 +1,8 @@
+package com.birette.repository.entity;
+
+import javax.persistence.Entity;
+
+@Entity(name="USER")
+public class User {
+
+}
